96e1f25116dbba50758c157597e8d55c69935abf
Stephen Thompson <s31@umbc.edu>
2021-10-31 09:27:31 -0500

    use get /api/notes to return all saved notes as json

--- Develop/server.js
+++ Develop/server.js
@@ -1,18 +1,25 @@
 // Include necessary packages
 const express = require('express');
 const path = require('path');
+const allNotes = require('./db/db.json');
 
 const app = express();
 const PORT = 3001;
 
 // middleware
 app.use(express.static('public'));
+app.use(express.json());
 
-// Create path
+// Create path to notes app
 app.get('/notes', (req, res) => res.sendFile(path.join(__dirname, 'public/notes.html'))
 );
 
-// Route everything else to index.html file
+// Return saved notes as JSON file
+app.get('/api/notes', (req, res) => {
+    return res.json(allNotes);
+});
+
+// Route all other client GET requests to index.html file
 app.get('*', (req, res) => res.sendFile(path.join(__dirname, 'public/index.html'))
 );
 
